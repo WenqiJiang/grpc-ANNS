efc6995be19b2f0c10275e8a879573d0ed9214ac
wenqijiang <wenqi.j@columbia.edu>
2022-10-07 09:10:43 +0200

    update ANN demo

---
+++ wenqi_python_projects/ANN-demo/client.py
@@ -0,0 +1,58 @@
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the GRPC helloworld.Greeter client."""
+
+from __future__ import print_function
+
+import logging
+
+import grpc
+import ANN_pb2
+import ANN_pb2_grpc
+
+import numpy as np
+import faiss
+
+
+def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+
+
+    d = 64                           # dimension
+    nq = 2
+    xq = np.random.random((nq, d)).astype('float32')
+    xq[:, 0] += np.arange(nq) / 1000.
+
+
+    topK = 100
+    nprobe = 1
+
+    with grpc.insecure_channel('localhost:50051') as channel:
+
+        stub = ANN_pb2_grpc.ANNStub(channel) # proto service name + Stub
+
+        # serialize
+        xq = xq.reshape(-1,).tolist()
+        request = ANN_pb2.SearchRequest(nq = nq, topK = topK, nprobe=nprobe, xq=xq)
+
+        response = stub.Search(request)
+        print("Result dist: ", response.results_dist)
+        print("Result ID: ", response.results_ID)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
---
+++ wenqi_python_projects/ANN-demo/server.py
@@ -0,0 +1,83 @@
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the GRPC helloworld.Greeter server."""
+
+from cgitb import reset
+from concurrent import futures
+import logging
+
+import grpc
+import ANN_pb2
+import ANN_pb2_grpc
+
+import numpy as np
+import faiss
+
+class ANN(ANN_pb2_grpc.ANNServicer): # Service name  + Servicer
+
+    print("Initiating index...")
+    # Faiss demo: https://github.com/facebookresearch/faiss/blob/main/tutorial/python/2-IVFFlat.py
+    d = 64                           # dimension
+    nb = 10000                       # database size
+    np.random.seed(1234)             # make reproducible
+    xb = np.random.random((nb, d)).astype('float32')
+    xb[:, 0] += np.arange(nb) / 1000.
+
+    nlist = 100
+    quantizer = faiss.IndexFlatL2(d)  # the other index
+    index = faiss.IndexIVFFlat(quantizer, d, nlist, faiss.METRIC_L2)
+
+    assert not index.is_trained
+    index.train(xb)
+    assert index.is_trained
+    index.add(xb)                  # add may be a bit slower as well
+    print("Finished initiating index.")
+
+    def __init__(self):
+        pass
+
+    # method same should be consistent with .proto file
+    def Search(self, request, context):
+
+        # interpret request
+        nq = request.nq
+        topK = request.topK
+        nprobe = request.nprobe
+        xq = request.xq
+        assert nq * self.d == len(xq)
+        xq = np.array(xq, dtype=np.float32).reshape((nq, self.d))
+
+        self.index.nprobe = nprobe
+        D, I = self.index.search(xq, topK)
+        
+        # serialize
+        D = D.reshape(-1,).tolist()
+        I = I.reshape(-1,).tolist()
+        
+        response = ANN_pb2.SearchResponse(results_ID=I, results_dist=D)
+        
+        return response 
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    ANN_pb2_grpc.add_ANNServicer_to_server(ANN(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
